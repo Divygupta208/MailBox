b65f18d5fb3df21458cc7a46110f34f14f6c4e94
Divy Gupta <divygupta208@gmail.com>
2024-03-18 18:25:12 +0530

    side bar expand

--- src/component/NeuButton.jsx
+++ src/component/NeuButton.jsx
@@ -18,24 +18,25 @@ const AnimatedHamburgerButton = () => {
         initial={false}
         animate={active ? "open" : "closed"}
         onClick={() => setActive((pv) => !pv)}
-        className={`absolute mt-3 ml-[-1.5rem] h-12 w-12 rounded-full bg-white transition-colors 
+        className={`absolute mt-3 ml-[-1.5rem] h-12 w-12 rounded-full bg-black transition-colors 
         
-        shadow-[-5px_-5px_10px_rgba(255,_255,_255,_0.8),_5px_5px_10px_rgba(0,_0,_0,_0.25)]
-        hover:shadow-[-1px_-1px_5px_rgba(255,_255,_255,_0.6),_1px_1px_5px_rgba(0,_0,_0,_0.3),inset_-2px_-2px_5px_rgba(255,_255,_255,_1),inset_2px_2px_4px_rgba(0,_0,_0,_0.3)]  `}
+        shadow-[-5px_-5px_10px_rgba(0,0,0,0.8),_5px_5px_10px_rgba(0,0,0,0.25)]
+hover:shadow-[-1px_-1px_5px_rgba(0,0,0,0.6),_1px_1px_5px_rgba(0,0,0,0.3),inset_-2px_-2px_5px_rgba(0,0,0,1),inset_2px_2px_4px_rgba(0,0,0,0.3)]
+  `}
       >
         <motion.span
           variants={VARIANTS.top}
-          className="absolute h-1 w-10 bg-violet-800"
+          className="absolute h-1 w-10 bg-violet-600"
           style={{ y: "-50%", left: "50%", x: "-50%", top: "35%" }}
         />
         <motion.span
           variants={VARIANTS.middle}
-          className="absolute h-1 w-10 bg-violet-800"
+          className="absolute h-1 w-10 bg-violet-600"
           style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
         />
         <motion.span
           variants={VARIANTS.bottom}
-          className="absolute h-1 w-5 bg-violet-800"
+          className="absolute h-1 w-5 bg-violet-600"
           style={{
             x: "-50%",
             y: "50%",
--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -6,6 +6,7 @@ import { BsFillInboxesFill } from "react-icons/bs";
 import { RiMailSendFill } from "react-icons/ri";
 import { RiDraftLine } from "react-icons/ri";
 import { FaTrashAlt } from "react-icons/fa";
+import { BsShieldFillX } from "react-icons/bs";
 import Navbar from "./Navbar";
 import { Link, Navigate } from "react-router-dom";
 import ComposeMailForm from "./ComposeMail";
@@ -21,7 +22,7 @@ const IconSideNav = () => {
   };
 
   return (
-    <div className="text-slate-100">
+    <div className={`text-slate-100`}>
       <SideNav
         expanded={expanded}
         expandSideBar={expandSideBar}
@@ -40,66 +41,113 @@ const SideNav = ({ expanded, expandSideBar, selected, setSelected }) => {
   };
 
   return (
-    // NOTE: In prod, you'd likely set height to h-screen and fix to the viewport
     <motion.nav
-      className={`h-[100vh] w-fit bg-[#dddddd] p-4 flex flex-col items-center gap-2 relative  `}
-      initial={{ width: "70px", x: -100 }} // Initial width of the sidebar
+      className={`h-[100vh] bg-[#3f3f3f] p-4 flex flex-col items-center gap-2 relative
+      `}
+      initial={{ width: "70px", x: -100 }}
       animate={{
-        width: expanded ? "250px" : "80px",
+        width: expanded ? "180px" : "70px",
+
         x: 0,
-      }} // Expanded or collapsed width
+      }}
       transition={{
-        width: { duration: 0.3, ease: "easeInOut" },
+        width: { duration: 0.4 },
         x: { duration: 1 },
-      }} // Animation duration and easing function
+      }}
     >
       <div onClick={expandSideBar}>
         <Example />
       </div>
 
-      <div className="flex gap-4 justify-center mt-20" onClick={showCompose}>
+      <div className="flex gap-5 justify-center mt-20" onClick={showCompose}>
         <Link to={"compose"}>
-          <NavItem selected={selected === 0} id={0} setSelected={setSelected}>
+          <NavItem
+            selected={selected === 0}
+            id={0}
+            setSelected={setSelected}
+            expanded={expanded}
+          >
             <TfiWrite />
+            {expanded && <span className="ml-2 text-sm">Compose</span>}
           </NavItem>
         </Link>
-        {expanded && <div className="bold">Tailwind css</div>}
       </div>
 
       <Link to={"inbox"}>
-        <NavItem selected={selected === 1} id={1} setSelected={setSelected}>
+        <NavItem
+          selected={selected === 1}
+          id={1}
+          setSelected={setSelected}
+          expanded={expanded}
+        >
           <BsFillInboxesFill />
+          {expanded && <span className="ml-2 text-sm">Inbox</span>}
         </NavItem>
       </Link>
 
       <Link to={"sent"}>
-        <NavItem selected={selected === 2} id={2} setSelected={setSelected}>
+        <NavItem
+          selected={selected === 2}
+          id={2}
+          setSelected={setSelected}
+          expanded={expanded}
+        >
           <RiMailSendFill />
+          {expanded && <span className="ml-2 text-sm">Sent</span>}
         </NavItem>
       </Link>
-      <NavItem selected={selected === 3} id={3} setSelected={setSelected}>
+
+      <NavItem
+        selected={selected === 3}
+        id={3}
+        setSelected={setSelected}
+        expanded={expanded}
+      >
         <RiDraftLine />
+        {expanded && <span className="ml-2 text-sm">Draft</span>}
       </NavItem>
-      <NavItem selected={selected === 4} id={4} setSelected={setSelected}>
+
+      <NavItem
+        selected={selected === 4}
+        id={4}
+        setSelected={setSelected}
+        expanded={expanded}
+      >
         <FaTrashAlt />
+        {expanded && <span className="ml-2 text-sm">Trash</span>}
+      </NavItem>
+
+      <NavItem
+        selected={selected === 5}
+        id={5}
+        setSelected={setSelected}
+        expanded={expanded}
+      >
+        <BsShieldFillX />
+        {expanded && <span className="ml-2 text-sm">Spam</span>}
       </NavItem>
     </motion.nav>
   );
 };
 
-const NavItem = ({ children, selected, id, setSelected }) => {
+const NavItem = ({ children, selected, id, setSelected, expanded }) => {
   return (
     <motion.button
-      className="p-3 text-xl bg-black hover:bg-[#594c67] rounded-3xl transition-colors relative"
+      className={`p-3 text-xl bg-black hover:bg-[#594c67] rounded-3xl transition-colors relative ${
+        expanded ? "ml-[-2rem]" : ""
+      }
+        `}
       onClick={() => setSelected(id)}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
     >
-      <span className="block relative z-10">{children}</span>
+      <span className={`${expanded ? "w-24" : ""}  relative flex z-10`}>
+        {children}
+      </span>
       <AnimatePresence>
         {selected && (
           <motion.span
-            className="absolute inset-0 rounded-md bg-[#6629cf] z-0"
+            className=" absolute inset-0 rounded-md bg-[#6629cf] z-[1] "
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
             exit={{ scale: 0 }}
